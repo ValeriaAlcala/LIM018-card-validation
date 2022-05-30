7643aaeefa0b9120036f97f7154e5bd9a249209a
Valeria <valeria@MacBook-Pro-de-Valeria.local>
2022-05-30 16:50:07 -0500

    JS hasta suma de arrar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
-import validator from './validator.js';
+import validator from './validator.js'; //va al incio
 
-console.log(validator);
+const boton1=document.getElementById("boton1");
+boton1.addEventListener("click",ventana1);
 
-function validarTarjeta(){
-      
-document.getElementbyId("numero-tarjeta")
+const botonbasico=document.getElementById("botonbasico");
+botonbasico.addEventListener("click",ventana2);
+
+const botonestandar=document.getElementById("botonestandar");
+botonestandar.addEventListener("click",ventana2);
+ 
+const botonpremium=document.getElementById("botonpremium");
+botonpremium.addEventListener("click",ventana2);
+
+function ventana1(){
+  document.getElementById("ventana1").style.display="none";
+  document.getElementById("ventana2").style.display="block";
 }
+
+function ventana2(){
+  document.getElementById("ventana2").style.display="none";
+  document.getElementById("ventana3").style.display="block";
+}
+
+  console.log(validator);
+
+//const ccvalida=()=>{
+
+  let num = ""
+  const validarNumeros = document.getElementById("numero-tarjeta");
+  validarNumeros.addEventListener("change", (event) => {
+  num = event.target.value;
+  })
+
+  const botondepago=document.getElementById("boton-pagar");
+  botondepago.addEventListener("click", (event) => {
+  event.preventDefault();
+  validator.isValid(num);
+  })
+
+  //let mensaje="";
+    //if(validator(validarNumeros)=)
+      //mensaje="Número de tarjeta válida";
+    //else 
+      //mensaje="Número de tarjeta inválido";
+    //botondepago.textContent=mensaje;
+
+//}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,31 @@
 const validator = {
-  // ...
-};
+  isValid: function (num) {
 
-export default validator;
+    let arraydenumerotarjeta=num.split(""); //transformando el numero obtenido en un array
+    console.log(arraydenumerotarjeta);
+
+    let reversed=arraydenumerotarjeta.reverse(""); //array en reversa
+    console.log(reversed);
+
+    let multiplicarnumeros=reversed; //multiplicar pares del array
+      for(let i=1; i<reversed.length; i=i+2){
+        reversed[i]=reversed[i]*2;
+      console.log(multiplicarnumeros); 
+      }
+    
+    let sumadecifras=multiplicarnumeros; //suma de cifras
+      for(let i=1; i<multiplicarnumeros.length; i=i+2){
+        if(multiplicarnumeros[i]>9)
+          multiplicarnumeros[i]-=9;
+      console.log(sumadecifras);
+    }
+
+    let sumatotal=sumadecifras;
+    let reducir=sumatotal.reduce((acc,item)=>{
+      return acc=acc+parseInt(item);
+    },0);
+    console.log(reducir);
+  }
+  
+}
+export default validator; //va al final
